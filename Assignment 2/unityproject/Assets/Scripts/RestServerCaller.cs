4294413edba2cd0dcd37ffb66f197121839d23bd
leoul <leo.ulhaas@inoptech.de>
2025-05-28 20:50:59 +0200

    ServerCaller partly taken from template

--- Assignment 2/unityproject/Assets/Scripts/RestServerCaller.cs
+++ Assignment 2/unityproject/Assets/Scripts/RestServerCaller.cs
@@ -1,18 +1,84 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 using UnityEngine;
+using UnityEngine.Networking;
+using WebSocketSharp;
 
-public class RestServerCaller : MonoBehaviour
+public class RestServerCaller : SingletonPersistant<RestServerCaller>
 {
-    // Start is called before the first frame update
-    void Start()
+    //delegate für eigens definierte Funktionen: parameter ServerMessage, return void
+    //werden nach dem fertigen ausführen eines REST-Calls aufgerufen, dürfen weggelassen werden
+    public delegate void ServerRequestCallBack(ServerMessage response);
+    
+    
+    //Coroutine-Starter für Rest-Calls
+    public void GenericSendCall(string url, Dictionary<string, object> values,
+        ServerRequestCallBack callback = null)
     {
-        
+        StartCoroutine(GenericSend(url, values, callback));
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GenericSendCall(string url, WWWForm form, ServerRequestCallBack callback = null)
     {
-        
+        StartCoroutine(GenericSend(url, form, callback));
     }
+
+    public void GenericRequestCall(string url, ServerRequestCallBack callback = null)
+    {
+        StartCoroutine(GenericRequest(url, callback));
+    }
+    
+    
+    //Tatsächliche REST_Calls, als Coroutine Aufrufen
+    
+    public IEnumerator GenericSend(string url, Dictionary<string, object> values,
+            ServerRequestCallBack callback = null)
+        {
+            //little safety check
+            if (!url.StartsWith(TemplateSettings.url))
+                url = TemplateSettings.url + url;
+            using (var www = UnityWebRequest.Post(url, JsonConvert.SerializeObject(values), "application/json"))
+            {
+                yield return www.SendWebRequest();
+                if (www.downloadHandler.text.IsNullOrEmpty())
+                    throw new Exception("Server did not respond. Is the server up? or does it receive the request?");
+
+                callback?.Invoke(JsonConvert.DeserializeObject<ServerMessage>(www.downloadHandler.text));
+            }
+        }
+
+        public IEnumerator GenericSend(string url, WWWForm form, ServerRequestCallBack callback = null)
+        {
+            //little safety check
+            if (!url.StartsWith(TemplateSettings.url))
+                url = TemplateSettings.url + url;
+       
+            using (var www = UnityWebRequest.Post(url, form))
+            {
+                yield return www.SendWebRequest();
+                if (www.downloadHandler.text.IsNullOrEmpty())
+                    throw new Exception("Server did not respond. Is the server up? or does it receive the request?");
+                Debug.Log(www.downloadHandler.text);
+                callback?.Invoke(JsonConvert.DeserializeObject<ServerMessage>(www.downloadHandler.text));
+            }
+        }
+
+        public IEnumerator GenericRequest(string url, ServerRequestCallBack callback = null)
+        {
+            //little safety check
+            if (!url.StartsWith(TemplateSettings.url))
+                url = TemplateSettings.url + url;
+            using (var www = UnityWebRequest.PostWwwForm(url, ""))
+            {
+                yield return www.SendWebRequest();
+                if (www.downloadHandler.text.IsNullOrEmpty())
+                    throw new Exception("Server did not respond. Is the server up? or does it receive the request?");
+                Debug.Log(www.downloadHandler.text);
+                callback?.Invoke(JsonConvert.DeserializeObject<ServerMessage>(www.downloadHandler.text));
+            }
+        }
+    
 }
