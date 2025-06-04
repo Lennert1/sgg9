e158f24b5522b67ba4039f20f9273ae4898da21a
CrawsCat <tatanekosai@mnet-mail.de>
2025-06-04 21:45:34 +0200

    example methods for REST-requests used by UI Elements

--- Assignment 2/unityproject/Assets/Scripts/ui/UI.cs
+++ Assignment 2/unityproject/Assets/Scripts/ui/UI.cs
@@ -1,13 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using gamelogic.ServerClasses;
 using UnityEngine;
+using Newtonsoft.Json;
 
 public class UI : MonoBehaviour
 {
     protected GameManager gameManager;
 
+    User data;
+
     void Awake()
     {
         gameManager = GameManager.GetGameManager();
+    }
+
+    void LoadUserData()
+    {
+        RestServerCaller.Instance.GenericRequestCall("", UserCallback);
+        Debug.LogWarning("Missing URL in method LoadUserData() in class UI");
+    }
+
+    public void UserCallback(ServerMessage response)
+    {
+        if (response.IsError())
+        {
+            Debug.Log("Error");
+            return;
+        }
+
+        User usr = JsonConvert.DeserializeObject<User>(response.message);
+
+        // process received data
     }
 }
