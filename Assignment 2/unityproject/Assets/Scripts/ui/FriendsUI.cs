1ba544da656034de9a0c18c88f7689002feedf8d
Safichan <safichan@gmx.de>
2025-06-25 13:47:34 +0200

    Mainmenu deleted

--- Assignment 2/unityproject/Assets/Scripts/ui/FriendsUI.cs
+++ Assignment 2/unityproject/Assets/Scripts/ui/FriendsUI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainMenuUI : UI
+public class FriendsUI : UI
 {
     // Start is called before the first frame update
     void Start()
