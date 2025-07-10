f702b3759aec3b7e1133c59f4fee3775e92acdc5
Safichan <safichan@gmx.de>
2025-07-10 16:34:51 +0200

    Login functionality implemented

--- Assignment 2/Server/DjangoCore/ServerClass.py
+++ Assignment 2/Server/DjangoCore/ServerClass.py
@@ -1,7 +1,42 @@
 from enum import Enum
 
-
+# Our temporary database for users
+# To create user just create an instance with all hard coded values :D
 class ServerClass(Enum):
-    uid = 1234
-    name = "Pony"
-    color = 0
+    PONY = (1234, "Pony", 10, 400, 10)
+    TIGER = (5678, "Tiger", 20, 100, 30)
+
+    def __init__(self, uid, username, level, gold, armorPoints):
+        self._uid = uid
+        self._username = username
+        self._level = level
+        self._gold = gold
+        self._armorPoints = armorPoints
+
+    @property
+    def uid(self):
+        return self._uid
+
+    @property
+    def username(self):
+        return self._username
+
+    @property
+    def level(self):
+        return self._level
+
+    @property
+    def gold(self):
+        return self._gold
+
+    @property
+    def armorPoints(self):
+        return self._armorPoints
+
+    @classmethod
+    def get_user_by_name(cls, username):
+        for user in cls:
+            if user.username.lower() == username.lower():
+                return user
+        return None
+
--- Assignment 2/Server/DjangoCore/api/REST/views.py
+++ Assignment 2/Server/DjangoCore/api/REST/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
-import ServerClass
+from ServerClass import ServerClass
 import json
 import utilities
 
@@ -21,8 +21,49 @@ def login(request):
 
     print("Name: ", name)
     print("UID: ", uid)
-    return  utilities.server_message_response("received","STATUS")
+    return utilities.server_message_response("received","STATUS")
 
+@csrf_exempt
+def update_color(request):
+    msg = json.dumps({
+        "color": f"{ServerClass.color}"
+    })
+    return utilities.server_message_response(msg,"DATA")
+
+# =============================== Stuff that Han did ========================================
+
+# Receives input from Login UI and checks if user exists
+# If yes a json is sent to the frontend
+@csrf_exempt
+def check_login(request):
+    if request.method != "POST":
+        return utilities.server_message_response("Not a POST request was sent", "405", status = 405)
+
+    # Checking the login based on the username
+    try:
+        data = json.loads(request.body)
+        username = data.get('name')
+    except json.JSONDecodeError:
+        return utilities.server_message_response("Invalid Json!", "400", status = 400)
+
+    # Class method. Searches all the instances and returns None if user does not exist
+    user = ServerClass.get_user_by_name(username)
+
+    if user is None:
+        return utilities.server_message_response("User was not found", "404", status=404)
+    else:
+        user_data = {
+            "name": user.username,
+            "uid": user.uid,
+            "lvl": user.level,
+            "gold": user.gold,
+            "armorPoints": user.armorPoints
+        }
+        # Sends a Json Response back to the frontend
+        return JsonResponse(user_data, status=200)
+
+# This method is not finished yet
+# This should add an instance to the ServerClass
 @csrf_exempt
 def register(request):
     if request.method == "POST":
@@ -31,7 +72,7 @@ def register(request):
             print("Request body raw:", request.body)
             name = data.get("name")
 
-            # The UID should be assigned by the server
+            # The UID should be assigned by the server I think
             # uid = data.get("uid")
 
             print(f"Name: {name}")
@@ -43,10 +84,5 @@ def register(request):
         return utilities.server_message_response("received","STATUS", status=405)
 
 
-@csrf_exempt
-def update_color(request):
-    msg = json.dumps({
-        "color": f"{ServerClass.color}"
-    })
-    return utilities.server_message_response(msg,"DATA")
+
 
