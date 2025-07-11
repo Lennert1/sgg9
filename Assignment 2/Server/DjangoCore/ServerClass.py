ae336bc196626d2b0f27456d709a18b156fea93e
Safichan <safichan@gmx.de>
2025-07-11 18:02:47 +0200

    Added cards to ServerClass

--- Assignment 2/Server/DjangoCore/ServerClass.py
+++ Assignment 2/Server/DjangoCore/ServerClass.py
@@ -3,15 +3,18 @@ from enum import Enum
 # Our temporary database for users
 # To create user just create an instance with all hard coded values :D
 class ServerClass(Enum):
-    PONY = (1234, "Pony", 10, 400, 10)
-    TIGER = (5678, "Tiger", 20, 100, 30)
+    PONY = (12340, "Pony", 10, 400, 10,
+            [(1, 1, 1), (2, 1, 1), (3, 3, 1), (4, 6, 16), (5, 1, 1), (6, 9, 16)])
+    TIGER = (5678, "Tiger", 20, 100, 30, [])
 
-    def __init__(self, uid, username, level, gold, armorPoints):
+    def __init__(self, uid, username, level, gold, armorPoints, listOfCards):
         self._uid = uid
         self._username = username
         self._level = level
         self._gold = gold
         self._armorPoints = armorPoints
+        # List of triples (type, lvl, count)
+        self._listOfCards = listOfCards
 
     @property
     def uid(self):
@@ -32,6 +35,10 @@ class ServerClass(Enum):
     @property
     def armorPoints(self):
         return self._armorPoints
+
+    @property
+    def listOfCards(self):
+        return self._listOfCards
 
     @classmethod
     def get_user_by_name(cls, username):
