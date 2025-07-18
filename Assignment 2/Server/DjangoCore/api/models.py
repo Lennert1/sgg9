d222af08bc9124cfcd1ae95ae1bb7a7e6759ae57
leoul <leo.ulhaas@inoptech.de>
2025-07-18 23:19:05 +0200

    added all models

--- Assignment 2/Server/DjangoCore/api/models.py
+++ Assignment 2/Server/DjangoCore/api/models.py
@@ -4,6 +4,8 @@ from django_mongodb_backend.fields import *
 from django_mongodb_backend.models import *
 # Create your models here.
 
+##################################-Embedded-##################################
+
 class Card(EmbeddedModel):
     id = ObjectIdAutoField(primary_key=True)
     type = models.IntegerField()
@@ -17,6 +19,14 @@ class Character(EmbeddedModel):
     hp = models.IntegerField()
     deck = EmbeddedModelArrayField(embedded_model=Card, null=True, blank=True)
 
+class Enemy(EmbeddedModel):
+    id = ObjectIdAutoField(primary_key=True)
+    type = models.IntegerField()
+    lvl = models.IntegerField()
+    hp = models.IntegerField()
+    deck = EmbeddedModelArrayField(embedded_model=Card, null=True, blank=True)
+
+##################################-Actual-##################################
 
 class User(models.Model):
     id = ObjectIdAutoField(primary_key=True)
@@ -31,3 +41,26 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Party(models.Model):
+    pid = ObjectIdAutoField(primary_key=True)
+    hp = models.IntegerField()
+    shield = models.IntegerField()
+    members = ArrayField(ObjectIdField, null=True, blank=True)
+    PoIIds = ArrayField(models.IntegerField(), null=True, blank=True)
+
+
+class BattleArena(models.Model):
+    id = ObjectIdAutoField(primary_key=True)
+    pid = ObjectIdField()
+    enemy = EmbeddedModelField(embedded_model=Enemy, null=True, blank=True)
+    rewardGold = models.IntegerField()
+    rewardCards = EmbeddedModelArrayField(embedded_model=Card, null=True, blank=True)
+    rewardUpgradePoints = models.IntegerField()
+    teamHP = models.IntegerField()
+    teamShield = models.IntegerField()
+    playerChecks = ArrayField(models.BooleanField(), null=True, blank=True)
+    playerCards = EmbeddedModelArrayField(embedded_model=Card, null=True, blank=True)
+    enemyCards = EmbeddedModelArrayField(embedded_model=Card, null=True, blank=True)
+    battleState = models.IntegerField()
